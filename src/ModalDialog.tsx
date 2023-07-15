c4e3fbe55a08ce594f87edc1b86111ca12be49c8
Christian Hvid <christian.hvid@gmail.com>
2023-07-15 18:45:12 +0200

    Introduced focus trap component.

--- src/ModalDialog.tsx
+++ src/ModalDialog.tsx
@@ -1,77 +1,34 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { SearchField } from "./SearchField";
+import { FocusTrap } from "./components/FocusTrap";
 
 export const ModalDialog: React.FC<{ visible: boolean; onClose: () => void }> = ({ visible, onClose }) => {
   const dialogRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (visible) {
-      setTimeout(() => {
-        if (window.document.activeElement) {
-          console.log("calling (window.document.activeElement as any).blur()", window.document.activeElement as any);
-          (window.document.activeElement as any).blur();
-        }
-      }, 100);
-
-      const modifiedElements: { element: any; oldState: any }[] = [];
-
-      const isInsideThisElement = (e: any) => {
-        while (true) {
-          e = e.parentElement;
-          if (e == dialogRef.current!) {
-            return true;
-          }
-          if (e == null) {
-            return false;
-          }
-        }
-      };
-
-      const disableElement = (element: any): any => {
-        const oldState = element.disabled;
-        element.disabled = true;
-        return oldState;
-      };
-
-      const restoreElement = (element: any, oldState: any) => {
-        element.disabled = oldState;
-      };
-
-      document.querySelectorAll("input, text, button, select, textarea, label, form").forEach((e: any) => {
-        if (!isInsideThisElement(e)) {
-          modifiedElements.push({ element: e, oldState: disableElement(e) });
-        }
-      });
-
-      return () => {
-        for (let p of modifiedElements) {
-          restoreElement(p.element, p.oldState);
-        }
-      };
-    }
-  }, [visible]);
   return (
     <>
       {visible && (
-        <div className="modal" onClick={e => e.target == dialogRef.current && onClose()} ref={dialogRef}>
-          <div className="dialog" role="dialog">
-            <div className="header">Foo</div>
-            <div className="content">
-              <p>Bar baz ...</p>
-              <form>
-                <label>
-                  Search thing
-                  <SearchField placeholder="First thing" size={40} />
-                </label>
-                <div className="row">
-                  <button onClick={() => onClose()}>OK</button>
-                  <button onClick={() => onClose()} className="is-link">
-                    Cancel
-                  </button>
-                </div>
-              </form>
+        <FocusTrap>
+          <div className="modal" onClick={e => e.target == dialogRef.current && onClose()} ref={dialogRef}>
+            <div className="dialog" role="dialog">
+              <div className="header">Foo</div>
+              <div className="content">
+                <p>Bar baz ...</p>
+                <form>
+                  <label>
+                    Search thing
+                    <SearchField placeholder="First thing" size={40} />
+                  </label>
+                  <div className="row">
+                    <button onClick={() => onClose()}>OK</button>
+                    <button onClick={() => onClose()} className="is-link">
+                      Cancel
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
+        </FocusTrap>
       )}
     </>
   );
---
+++ src/components/FocusTrap.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef, PropsWithChildren } from "react";
+
+export const FocusTrap: React.FC<PropsWithChildren> = ({ children }) => {
+  const elementRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    setTimeout(() => {
+      if (window.document.activeElement) {
+        (window.document.activeElement as any).blur();
+      }
+    }, 100);
+
+    const modifiedElements: { element: any; oldState: any }[] = [];
+
+    const isInsideThisElement = (e: any) => {
+      while (true) {
+        e = e.parentElement;
+        if (e == elementRef.current!) {
+          return true;
+        }
+        if (e == null) {
+          return false;
+        }
+      }
+    };
+
+    const disableElement = (element: any): any => {
+      const oldState = element.disabled;
+      element.disabled = true;
+      return oldState;
+    };
+
+    const restoreElement = (element: any, oldState: any) => {
+      element.disabled = oldState;
+    };
+
+    document.querySelectorAll("input, text, button, select, textarea, label, form").forEach((e: any) => {
+      if (!isInsideThisElement(e)) {
+        modifiedElements.push({ element: e, oldState: disableElement(e) });
+      }
+    });
+
+    return () => {
+      for (let p of modifiedElements) {
+        restoreElement(p.element, p.oldState);
+      }
+    };
+  }, []);
+  return (
+    <div ref={elementRef}>{children}</div>
+  );
+};
